75d7db1f7a3f0d89569d5ad4f1605adcc3827fe0
Albert Chua <66683333+blargeth@users.noreply.github.com>
2021-03-12 12:40:21 -0500

    Time: 80 ms (45.09%), Space: 38.4 MB (69.94%) - LeetHub

---
+++ number-of-segments-in-a-string/number-of-segments-in-a-string.js
@@ -0,0 +1,10 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var countSegments = function(s) {
+  if (s === '') {
+    return 0;
+  }
+    return s.split(' ').filter(letter => letter !== '').length;
+};
