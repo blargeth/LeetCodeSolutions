f8780830d47ab1292530411fb70b5242c37c7aee
Albert Chua <66683333+blargeth@users.noreply.github.com>
2021-05-22 11:17:50 -0400

    Time: 132 ms (25.95%), Space: 42.4 MB (84.18%) - LeetHub

--- delete-columns-to-make-sorted/delete-columns-to-make-sorted.js
+++ delete-columns-to-make-sorted/delete-columns-to-make-sorted.js
@@ -6,7 +6,7 @@ var minDeletionSize = function(strs) {
     let result = 0;
     for (let j = 0; j < strs[0].length; j++) {
       for (let i = 0; i < strs.length - 1; i++) {
-        console.log(strs[i].charAt(j), strs[i + 1].charAt(j))
+        // console.log(strs[i].charAt(j), strs[i + 1].charAt(j))
         if (strs[i].charAt(j) > strs[i + 1].charAt(j)) {
           result += 1;
           break;
