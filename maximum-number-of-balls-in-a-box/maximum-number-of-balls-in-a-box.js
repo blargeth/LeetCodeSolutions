6044e9e49b97618c691eaee35585abb8607054f4
Albert Chua <66683333+blargeth@users.noreply.github.com>
2021-06-12 00:50:25 -0400

    Time: 1456 ms (5.20%), Space: 46.6 MB (66.00%) - LeetHub

---
+++ maximum-number-of-balls-in-a-box/maximum-number-of-balls-in-a-box.js
@@ -0,0 +1,36 @@
+/**
+ * @param {number} lowLimit
+ * @param {number} highLimit
+ * @return {number}
+ */
+var countBalls = function(lowLimit, highLimit) {
+    
+  let toDigitSum = (number) => {
+    let sum = 0;
+    while (number >= 1) {
+      let digit = number % 10;
+      sum += digit;     
+      number = (number - digit ) / 10;
+    }
+    return sum;
+  }
+  
+  let hashbrown = {};
+  
+  for (let i = lowLimit; i <= highLimit; i++) {
+    let sum = toDigitSum(i);
+    console.log(sum)
+    if (!hashbrown[sum]) {
+      hashbrown[sum] = 1
+    } else {
+      hashbrown[sum] += 1;
+    }
+  }
+
+  let result = 0;
+  for (let key in hashbrown) {
+    if (hashbrown[key] > result) result = hashbrown[key];
+  }
+
+  return result;
+};
