27f53a0318b919b1139b681227940f69020170bf
Albert Chua <66683333+blargeth@users.noreply.github.com>
2021-06-19 21:15:22 -0400

    Time: 6304 ms (5.66%), Space: 119.6 MB (7.55%) - LeetHub

--- find-k-th-smallest-pair-distance/find-k-th-smallest-pair-distance.js
+++ find-k-th-smallest-pair-distance/find-k-th-smallest-pair-distance.js
@@ -5,23 +5,24 @@
  */
 var smallestDistancePair = function(nums, k) {
   //initialize distances with the max number of distances that can be created by the integers in nums
-  let distanceCount = new Array( Math.max(...nums)).fill(0);
-  //fill the array with the count of the distances
+  let distanceCount = {};
+  nums.sort( (a, b) => a - b);
+  
   for (let i = 0; i < nums.length - 1; i++) {
     for (let j = i + 1; j < nums.length; j++) {
-      distanceCount[Math.abs(nums[i] - nums[j])] += 1;
+      distanceCount[Math.abs(nums[i] - nums[j])] = (distanceCount[Math.abs(nums[i] - nums[j])] === undefined) ? 1 : distanceCount[Math.abs(nums[i] - nums[j])] += 1;
     }
   }
   // console.log(distanceCount)
   let result = k;
   
-  for (let i = 0; i < distanceCount.length; i++) {
-    if (distanceCount[i] > 0) {
-      result = result - distanceCount[i];
+  for (let key in distanceCount) {
+    if (distanceCount[key] > 0) {
+      result = result - distanceCount[key];
 
     }
     if (result <= 0) {
-      return i;
+      return key;
     }
     // console.log(i, result)
   }
