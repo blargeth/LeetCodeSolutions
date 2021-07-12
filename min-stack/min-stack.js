5ad09618bf13c73a77abe3ad59f885140fae0aed
Albert Chua <66683333+blargeth@users.noreply.github.com>
2021-07-12 13:47:11 -0400

    Time: 148 ms (41.09%), Space: 48.2 MB (8.45%) - LeetHub

--- min-stack/min-stack.js
+++ min-stack/min-stack.js
@@ -2,6 +2,7 @@
  * initialize your data structure here.
  */
 var MinStack = function() {
+  // data is a couplet of value and min value: [num, min value tracker]
   this.data = [];
 };
 
@@ -10,29 +11,35 @@ var MinStack = function() {
  * @return {void}
  */
 MinStack.prototype.push = function(x) {
-  this.data.push(x);
+
+  if (this.data.length === 0) { 
+    this.data.push([x,x]);
+    return;
+  } else {
+    this.data.push([x, Math.min(this.data[this.data.length - 1][1], x)]);
+  }
+  // console.log(this.data)
 };
 
 /**
  * @return {void}
  */
 MinStack.prototype.pop = function() {
-  this.data.pop(); 
+  this.data.pop();
 };
 
 /**
  * @return {number}
  */
 MinStack.prototype.top = function() {
-  return this.data[this.data.length - 1]
+  return this.data[this.data.length - 1][0]
 };
 
 /**
  * @return {number}
  */
 MinStack.prototype.getMin = function() {
-  let copy = this.data.slice().sort((a,b) => a - b);
-  return copy[0];
+  return this.data[this.data.length - 1][1]
 };
 
 /** 
