274081c299066f50d94558c3788a75356334c1fb
Albert Chua <66683333+blargeth@users.noreply.github.com>
2021-07-13 01:50:31 -0400

    Time: 180 ms (5.33%), Space: 41.5 MB (28.53%) - LeetHub

--- destination-city/destination-city.js
+++ destination-city/destination-city.js
@@ -4,14 +4,30 @@
  */
 var destCity = function(paths) {
     
-  const mapPaths = new Map(paths);
-  
-  for (let key of mapPaths) {
-    let start = key[1];
-    while (mapPaths.has(start)) {
-      start = mapPaths.get(start);
-    }
-    return start;
-  }
+  // const mapPaths = new Map(paths);
+  // console.log(mapPaths)
+  // for (let key of mapPaths) {
+  //   let start = key[1];
+  //   while (mapPaths.has(start)) {
+  //     start = mapPaths.get(start);
+  //   }
+  //   return start;
+  // }
 
+  if (paths.length === 1) {
+    return paths[0][1];
+    
+  }
+  
+  let map = {};
+  for (let i = 0; i < paths.length; i++) {
+    if (!map[paths[i][0]]) map[paths[i][0]] = 1;
+    else map[paths[i][0]]++;
+    if (!map[paths[i][1]]) map[paths[i][1]] = -1;
+    else map[paths[i][1]]--;
+    
+  }
+  for (let key in map) {
+    if (map[key] === -1) return key
+  }
 };
