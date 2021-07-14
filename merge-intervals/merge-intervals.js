f7140d11edb24b63c893d21e154d43fb33869781
Albert Chua <66683333+blargeth@users.noreply.github.com>
2021-07-14 02:43:08 -0400

    Time: 104 ms (28.80%), Space: 41.8 MB (19.94%) - LeetHub

--- merge-intervals/merge-intervals.js
+++ merge-intervals/merge-intervals.js
@@ -3,6 +3,46 @@
  * @return {number[][]}
  */
 var merge = function(intervals) {
+ 
+  // sort the intergals by beg[i] 
+  // iterate thru each interval starting at the index = 1
+  // check if the end of i - 1 === the beginning of i
+  // splice the interval array to replace the 2 intervals with the new 1
+  
+  
+//   intervals.sort( (a, b) => a[0] - b[0])
+
+//   for (let i = 1; i < intervals.length; i++) {
+//     if ( intervals[i-1][1] >= intervals[i][0] ) {
+//       intervals.splice(i - 1, 2, [intervals[i-1][0], Math.max(intervals[i][1], intervals[i-1][1] )])
+//       i--;
+//     }
+//     return intervals;
+//   }
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
   intervals.sort( (a,b) => a[0] - b[0]);
   
   for (let i = 1; i < intervals.length; i++) {
