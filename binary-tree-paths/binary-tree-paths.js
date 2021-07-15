0c96aa9a8fe0b1bf5391e905707e5279de0bdc36
Albert Chua <66683333+blargeth@users.noreply.github.com>
2021-07-14 20:14:13 -0400

    Time: 84 ms (56.39%), Space: 40.3 MB (59.31%) - LeetHub

--- binary-tree-paths/binary-tree-paths.js
+++ binary-tree-paths/binary-tree-paths.js
@@ -12,27 +12,82 @@
  */
 
 
+// traverse the tree when u get to a leaf with no children, then u return that path 
+
+//i want something that will store the path to that leaf 
+
 var binaryTreePaths = function(root) {
+  
   
   let result = [];
   
-  let traverse = ( node, currPath ) => {
-    if ( !node ) {
-      return;
-    };
+  const traverse = (node, currentPath) => {
+    // stop condition
+    if (!node) return;
     
-    if (node.left === null && node.right === null) {
-      currPath += node.val;
-      result.push(currPath);
-      return;
+    currentPath.push(node.val);
+    
+    if (!node.left && !node.right) {
+      let res = currentPath.join('->');
+      result.push(res);
     }
-    traverse( node.left, currPath + node.val + "->");
-    traverse( node.right, currPath + node.val + "->");
     
+    // console.log(currentPath)
+    //recursion
+    traverse(node.left, [...currentPath])
+    traverse(node.right, [...currentPath])
   }
   
-  traverse(root,'');
+  traverse(root, []);
   
   return result;
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+//   let result = [];
+//   let pathTrack = [];
+  
+//   let traverse = ( node ) => {
+//     if ( !node ) {
+//       return;
+//     };
+    
+//     pathTrack.push(node.val);
+    
+//     if (node.left === null && node.right === null) {
+//       // pathTrack.push(node.val);
+//       result.push(pathTrack.join('->'));
+//     } else {
+//       traverse( node.left);
+//       traverse( node.right);      
+//     }
+    
+//     pathTrack.pop();
+//   }
+  
+//   traverse(root, []);
+  
+//   return result;
   
 };
