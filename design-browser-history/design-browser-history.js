374a894fa139d4f95617fea634a9f19f5b20a0be
Albert Chua <66683333+blargeth@users.noreply.github.com>
2021-07-15 08:08:29 -0400

    Time: 204 ms (56.83%), Space: 49.8 MB (21.59%) - LeetHub

--- design-browser-history/design-browser-history.js
+++ design-browser-history/design-browser-history.js
@@ -1,19 +1,20 @@
 /**
  * @param {string} homepage
  */
-var BrowserHistory = function(homepage) {
-   this.history = [homepage];
-   this.currentIndex = 0;
+var BrowserHistory = function(homepage) {  
+  this.history = [homepage];
+  this.currentIndex = 0;
 };
 
 /** 
  * @param {string} url
  * @return {void}
  */
-BrowserHistory.prototype.visit = function(url) { 
-  this.currentIndex+= 1;
-  this.history = this.history.slice(0, this.currentIndex);
+BrowserHistory.prototype.visit = function(url) {
+  //clear all the forward history by using length
+  this.history.length = this.currentIndex + 1;
   this.history.push(url);
+  this.currentIndex += 1;
 };
 
 /** 
@@ -21,10 +22,10 @@ BrowserHistory.prototype.visit = function(url) {
  * @return {string}
  */
 BrowserHistory.prototype.back = function(steps) {
-  let newI = Math.max(this.currentIndex - steps, 0)
-    // console.log(this.currentIndex)
-  this.currentIndex = newI;
-  return this.history[newI];
+
+  const newIndex = Math.max(0, this.currentIndex - steps);
+  this.currentIndex = newIndex;
+  return this.history[newIndex];
 };
 
 /** 
@@ -32,9 +33,10 @@ BrowserHistory.prototype.back = function(steps) {
  * @return {string}
  */
 BrowserHistory.prototype.forward = function(steps) {
-  let newI = Math.min(this.currentIndex + steps, this.history.length - 1);
-  this.currentIndex = newI;
-  return this.history[newI];
+
+  const newIndex = Math.min(this.history.length - 1, this.currentIndex + steps);
+  this.currentIndex = newIndex;
+  return this.history[newIndex];
 };
 
 /** 
