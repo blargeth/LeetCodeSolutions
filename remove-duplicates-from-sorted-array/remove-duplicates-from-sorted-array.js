90ce9744998c3cf0a504561414c91174257f7f9f
Albert Chua <66683333+blargeth@users.noreply.github.com>
2021-08-12 14:24:01 -0400

    Time: 92 ms (52.95%), Space: 41.6 MB (34.58%) - LeetHub

---
+++ remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var removeDuplicates = function(nums) {
+  if (nums.length === 0) return 0;
+  let length = nums.length;
+  let counter = 0;
+  let dupes = 0;
+  
+  for (let i = 0; i < length - 1; i++) {
+    if (nums[i] !== nums[i + 1]) {
+      counter += 1;
+      nums[counter] = nums[i+1]; 
+    } else {
+      dupes += 1;
+    }
+  }
+  console.log(counter)
+  return nums.length - dupes;
+};
