11f65cc352ac77afc329d262d4acb87734a294e8
Albert Chua <66683333+blargeth@users.noreply.github.com>
2021-09-14 10:04:08 -0400

    Time: 132 ms (36.60%), Space: 50.3 MB (63.18%) - LeetHub

--- product-of-array-except-self/product-of-array-except-self.js
+++ product-of-array-except-self/product-of-array-except-self.js
@@ -3,6 +3,23 @@
  * @return {number[]}
  */
 var productExceptSelf = function(nums) {
+  
+  
+  
+  
+  
+  
+  
+  ////////// SOLUTION///////////////////
+  
+  //pseudo:
+  // create a result array
+  // create a variable that calculates the product of all previous iterations
+  // iterate thru and update each item in the list with the product
+  // reinitialize variable to calculate the product going backwards
+  // iterate backwards
+  
+  
   let products = Array(nums.length).fill(1)
   let product = 1;
   for (let i = 0; i < nums.length; i++) {
