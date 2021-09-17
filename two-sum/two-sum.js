3077e8ace4eb5c9ae526162a52596ef540c47d09
Albert Chua <66683333+blargeth@users.noreply.github.com>
2021-09-16 22:23:52 -0400

    Time: 68 ms (97.76%), Space: 40.6 MB (35.29%) - LeetHub

--- two-sum/two-sum.js
+++ two-sum/two-sum.js
@@ -4,13 +4,64 @@
  * @return {number[]}
  */
 var twoSum = function(nums, target) {
-  const map = new Map();
+  
+  
+  // create an object to hold the complement of the sum
+  // iterate thru each item in nums
+    // check if the current item is greater than 0 (if it exists)
+      // if it does, return the indexes
+  // set the complement equal to the index
+  
+  
+  let obj = {};
   
   for (let i = 0; i < nums.length; i++) {
-    let complement = target - nums[i];
-    if (map.has(complement)) {
-      return [map.get(complement), i];
-    }
-    map.set(nums[i], i);
+    let num = nums[i];
+    if (obj[num] >= 0) return [obj[num], i];
+    obj[target - num] = i;
   }
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  //i array and integer
+  //o couplet - with the indices of the solution
+  //c there will ALWAYS be a solution, cant use 1 item twice
+  //e a number in nums is greater than target? can possibly disregard
+  
+  
+  // [ 15    2   11   7    1   ]  target is 9
+  //   x                   y             
+  // { 2: [7, 0], 11: [-2, 1], 15: [-6, 2],          .... "number": [nums[i], i]  }    [3, 0]
+   
+  // by using a map that will store the complements and the index
+  
+
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+//   const map = new Map();
+  
+//   for (let i = 0; i < nums.length; i++) {
+//     let complement = target - nums[i];
+//     if (map.has(complement)) {
+//       return [map.get(complement), i];
+//     }
+//     map.set(nums[i], i);
+//   }
 };
