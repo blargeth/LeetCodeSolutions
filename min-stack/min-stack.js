13f105fe08425d9c4725a2793e5ca30b799fa4e8
Albert Chua <66683333+blargeth@users.noreply.github.com>
2021-10-25 18:54:40 -0400

    Time: 413 ms (8.84%), Space: 46.4 MB (55.61%) - LeetHub

--- min-stack/min-stack.js
+++ min-stack/min-stack.js
@@ -1,51 +1,42 @@
-/**
- * initialize your data structure here.
- */
+
 var MinStack = function() {
-  // data is a couplet of value and min value: [num, min value tracker]
-  this.data = [];
+    this.stack = [];
 };
 
 /** 
- * @param {number} x
+ * @param {number} val
  * @return {void}
  */
-MinStack.prototype.push = function(x) {
-
-  if (this.data.length === 0) { 
-    this.data.push([x,x]);
-    return;
-  } else {
-    this.data.push([x, Math.min(this.data[this.data.length - 1][1], x)]);
-  }
-  // console.log(this.data)
+MinStack.prototype.push = function(val) {
+    this.stack.push(val);
 };
 
 /**
  * @return {void}
  */
 MinStack.prototype.pop = function() {
-  this.data.pop();
+    return this.stack.pop();
 };
 
 /**
  * @return {number}
  */
 MinStack.prototype.top = function() {
-  return this.data[this.data.length - 1][0]
+    let len = this.stack.length - 1;
+    return this.stack[len];
 };
 
 /**
  * @return {number}
  */
 MinStack.prototype.getMin = function() {
-  return this.data[this.data.length - 1][1]
+    return Math.min(...this.stack)
 };
 
 /** 
  * Your MinStack object will be instantiated and called as such:
  * var obj = new MinStack()
- * obj.push(x)
+ * obj.push(val)
  * obj.pop()
  * var param_3 = obj.top()
  * var param_4 = obj.getMin()
